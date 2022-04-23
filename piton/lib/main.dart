d2222ea32b7971663f3c6662bd4b84e45fc49bf8
Adil Moumni Abdou <moumniabdou.adil@gmail.com>
2022-04-23 01:20:04 +0000

    a

--- piton/lib/main.dart
+++ piton/lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'add_user.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
